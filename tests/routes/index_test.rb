687929dea4ab27d89e90cee54a3ffa479ad0ce07
Steven Alexander <steven.william.alexander@googlemail.com>
2013-02-27 12:31:29 +0000

    added basic styles and content;

--- tests/routes/index_test.rb
+++ tests/routes/index_test.rb
@@ -4,6 +4,7 @@ class IndexTest < Test::Unit::TestCase
   def test_it_says_index
     get "/"
     assert last_response.ok?
-    assert last_response.body.include?('Index')
+    assert last_response.body.include?('href="/tv"')
+    assert last_response.body.include?('href="/radio"')
   end
 end

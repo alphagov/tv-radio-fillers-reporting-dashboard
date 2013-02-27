387124c03edeb7938ccebb81bb4acd5281d8f9a7
Steven Alexander <steven.william.alexander@googlemail.com>
2013-02-27 15:01:40 +0000

    updated breadcrumbs;

--- app/routes/dashboard.rb
+++ app/routes/dashboard.rb
@@ -1,8 +1,8 @@
 
 get '/tv' do
-  erb :dashboard, :locals => { :mode => "tv" }
+  erb :dashboard, :locals => { :mode => "tv", :breadcrumbs => [{ :label => "Home", :link => "/"}, { :label => "TV", :link => "/tv"}] }
 end
 
 get '/radio' do
-  erb :dashboard, :locals => { :mode => "radio" }
+  erb :dashboard, :locals => { :mode => "radio", :breadcrumbs => [{ :label => "Home", :link => "/"}, { :label => "Radio", :link => "/radio"}] }
 end

9dc9c7d6d69b6debf82a333fb7635724f74bec6f
Steven Alexander <steven.william.alexander@googlemail.com>
2013-02-27 13:21:08 +0000

    updated test name

--- tests/routes/index_test.rb
+++ tests/routes/index_test.rb
@@ -1,7 +1,7 @@
 require_relative '../test_helper.rb'
 
 class IndexTest < Test::Unit::TestCase
-  def test_it_says_index
+  def test_it_has_links_to_tv_radio
     get "/"
     assert last_response.ok?
     assert last_response.body.include?('href="/tv"')

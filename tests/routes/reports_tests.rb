de6f4c51d4bd9970bec149f5789b9995a3cc848e
Steven Alexander <steven.william.alexander@googlemail.com>
2013-02-28 17:07:18 +0000

    added basic criteria

--- tests/routes/reports_tests.rb
+++ tests/routes/reports_tests.rb
@@ -7,4 +7,15 @@ class ReportsTest < Test::Unit::TestCase
   def test_it_ok_for_mode_radio
     it_ok("/radio/reports")
   end
+  
+  def test_has_criteria
+    has_criteria('tv')
+    has_criteria('radio')
+  end
+  
+  def has_criteria(mode)
+    get "/#{mode}/reports"
+    assert last_response.body.include?('name="filler_name"')
+    assert last_response.body.include?('name="station_name"')
+  end
 end

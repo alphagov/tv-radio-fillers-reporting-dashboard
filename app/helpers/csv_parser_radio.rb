e642eace9f87aa91fafb5fff2098b198c1f5df16
Steven Alexander <steven.william.alexander@googlemail.com>
2013-02-28 11:26:32 +0000

    added radio csv parser

---
+++ app/helpers/csv_parser_radio.rb
@@ -0,0 +1,60 @@
+require 'csv'
+require 'date'
+require "logger"
+require "fileutils"
+require_relative "../models/fillerEntry.rb"
+
+class CsvParserRadio
+  
+  DATE_FORMAT = '%d-%b-%y'
+  CSV_PARSER_RADIO_LOG = "logs/CsvParserRadio.log"
+  
+  def parse_radio_row(row)
+    raise ArgumentError, "Too few rows", caller if row.length < 4
+
+    date_string = row[3]
+    date = nil
+    begin
+      date = DateTime.strptime(date_string, DATE_FORMAT)
+    rescue Exception => e
+      raise ArgumentError, "Could not parse date: #{date_string}", caller
+    end
+
+    FillerEntry.new(
+     :type => "radio",
+     :date => date,
+     :time_of_day => row[4],
+     :station_name => row[1],
+     :impact => row[2],
+     :filler_name => row[0]
+    )
+  end
+  
+  def parse_radio_file(file_full_path)
+    FileUtils.rm(CSV_PARSER_RADIO_LOG) if File.exists?(CSV_PARSER_RADIO_LOG)
+    log = Logger.new(CSV_PARSER_RADIO_LOG)
+    log.level = Logger::DEBUG
+
+    raise ArgumentError, "File does not exist", caller if !File.exists?(file_full_path)
+
+    filler = nil
+    row_index = 0
+    parsed_count = 0
+    CSV.foreach(file_full_path, { :encoding => "iso-8859-1:utf-8" }) do |row|
+      begin
+        self.parse_radio_row(row).save
+        parsed_count += 1
+      rescue Exception => e
+        log.info "INVALID ROW: #{row_index} - #{row.to_s} - #{e.message}"
+      end
+
+      if row_index % 100 == 0 && row_index > 0
+        log.info "Parsed #{row_index} rows"
+      end
+      row_index += 1
+    end
+    
+    log.info "Finished parsing: parsed #{parsed_count} rows"
+  end
+  
+end
---
+++ tests/helpers/csv_parser_radio_test.rb
@@ -0,0 +1,53 @@
+require_relative '../../app/helpers/csv_parser_radio.rb'
+require_relative '../../app/models/fillerEntry.rb'
+require 'test/unit'
+require 'csv'
+
+class CsvParserRadioTest < Test::Unit::TestCase
+  
+  DATE_FORMAT = '%d-%b-%y'
+  
+  def setup
+    Mongoid.load!("mongoid.yml", :test)
+    FillerEntry.delete_all
+    
+    @csv_parser = CsvParserRadio.new
+    
+    @sample_file_full_path = "tests/helpers/csv_samples/radio_fillers_sample.csv"
+  end
+  
+  def test_parse_radio_row
+    sample_row = ["CB00003030 Dance On 30s", "Absolute 70s", "2767", "17-Jan-13", "21:34"]
+    filler = @csv_parser.parse_radio_row(sample_row)
+    
+    assert !filler.nil?
+    assert filler.type == "radio"
+    assert filler.date == DateTime.strptime(sample_row[3], DATE_FORMAT)
+    assert filler.time_of_day == sample_row[4]
+    assert filler.station_name == sample_row[1]
+    assert filler.impact == sample_row[2]
+    assert filler.filler_name == sample_row[0]
+  end
+  
+  def def test_parse_radio_row_too_few_rows
+    invalid_row = ["Absolute 70s", "2767", "17-Jan-13", "21:34"]
+    
+    exception = assert_raise(ArgumentError) { @csv_parser.parse_radio_row(invalid_row) }
+    assert_equal("Too few rows", exception.message)
+  end
+  
+  def test_parse_radio_row_invalid_date
+    invalid_row = ["CB00003030 Dance On 30s", "Absolute 70s", "2767", "01/31/2013", "21:34"]
+    
+    exception = assert_raise(ArgumentError) { @csv_parser.parse_radio_row(invalid_row) }
+    assert_equal("Could not parse date: #{invalid_row[3]}", exception.message)
+  end
+  
+  def test_parse_radio_sample_file
+    @csv_parser.parse_radio_file(@sample_file_full_path)
+    
+    fillers = FillerEntry.where(type: "radio")
+    assert fillers.count == 12
+  end
+
+end

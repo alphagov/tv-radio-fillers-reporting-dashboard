fb4dba50db42d364e334552d8fe5cf1de215aa3c
Steven Alexander <steven.william.alexander@googlemail.com>
2013-03-01 11:05:11 +0000

    fixed reports link

--- app/routes/generate_report.rb
+++ app/routes/generate_report.rb
@@ -4,7 +4,7 @@ get '/tv/generate-report' do
 end
 
 get '/radio/generate-report' do
-  erb :generate_report, :locals => { :mode => "radio", :breadcrumbs => get_breadcrumbs_radio([{ :label => "Reports", :link => "/tv/reports"}]), :data => query_for_params('radio', params) }
+  erb :generate_report, :locals => { :mode => "radio", :breadcrumbs => get_breadcrumbs_radio([{ :label => "Reports", :link => "/radio/reports"}]), :data => query_for_params('radio', params) }
 end
 
 def query_for_params(mode, params)

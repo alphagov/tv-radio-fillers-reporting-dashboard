70f4d9891f3438b87d86d805e86ab82ea4c7696d
Steven Alexander <steven.william.alexander@googlemail.com>
2013-03-04 09:28:48 +0000

    added json method

--- app/routes/api.rb
+++ app/routes/api.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'json'
 
 get '/api/:mode/download-csv' do
   content_type "text/csv"
@@ -11,4 +12,11 @@ get '/api/:mode/download-csv' do
       csv << [filler.type, filler.date, filler.time_of_day, filler.station_name, filler.impact, filler.spot_value, filler.client_name, filler.filler_name, filler.campaign_name, filler.clock_name, filler.not_for_profit_station]
     end
   end
+end
+
+get  '/api/:mode/download-json' do
+  content_type :json
+  results = query_for_params(params[:mode], params)
+  
+  results.to_json
 end

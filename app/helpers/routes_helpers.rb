f3dd134a1332c218ae19dd034898e6c3144bc95a
Steven Alexander <steven.william.alexander@googlemail.com>
2013-03-07 10:02:16 +0000

    updated reports page for list of reports

--- app/helpers/routes_helpers.rb
+++ app/helpers/routes_helpers.rb
@@ -5,15 +5,15 @@ require_relative '../../app/models/transmission.rb'
 module RoutesHelpers
 
   REPORTS = [
-    { :name => "2B: Client Reports, Transmission by Station and Time of Day" },
-    { :name => "2C: Client Reports, Transmission by Time of Day" },
-    { :name => "3A: Client Reports, Transmission by Filler and Station" },
-    { :name => "4A: Summary Reports, Client by Station Type and Time of Day" },
-    { :name => "4B: Summary Reports, Client by Station Type, Theme and Filler" },
-    { :name => "5B: Top Terrestial Transmissions" },
-    { :name => "M1: Clearance Expiry List" },
-    { :name => "Client Report Set per Client" },
-    { :name => "Annual Tracking by Client" }
+    { :name => "2B: Client Reports, Transmission by Station and Time of Day"  , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "2C: Client Reports, Transmission by Time of Day"              , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "3A: Client Reports, Transmission by Filler and Station"       , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "4A: Summary Reports, Client by Station Type and Time of Day"  , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "4B: Summary Reports, Client by Station Type, Theme and Filler", :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "5B: Top Terrestial Transmissions"                             , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "M1: Clearance Expiry List"                                    , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "Client Report Set per Client"                                 , :value => "3a_client_reports_transmission_by_filler_and_station" },
+    { :name => "Annual Tracking by Client"                                    , :value => "3a_client_reports_transmission_by_filler_and_station" }
   ]
 
   def is_param_not_nil_empty(params, param_name)

6e244c788b55c5898c44e85c56426ec161de4fc4
Steven Alexander <steven.william.alexander@googlemail.com>
2013-03-08 11:40:34 +0000

    added fillers/station import routes

--- app/routes/import.rb
+++ app/routes/import.rb
@@ -3,6 +3,7 @@ get '/:mode/import' do
   erb :import, :locals => { :mode => params[:mode], :breadcrumbs => get_breadcrumbs_by_mode(params[:mode], [{ :label => "Import", :link => "/#{params[:mode]}/import"}]) }
 end
 
+
 get '/:mode/import/transmissions' do
   instructions = params[:mode] == 'tv' ? CsvParserTv::INSTRUCTION_TEXT : CsvParserRadio::INSTRUCTION_TEXT
   erb :"import/import_csv", :locals => { :mode => params[:mode], :upload_type => 'Transmissions', :instructions => instructions, :breadcrumbs => get_breadcrumbs_by_mode(params[:mode], [{ :label => "Import", :link => "/#{params[:mode]}/import"}, { :label => "Transmissions", :link => "/#{params[:mode]}/import/transmissions"}]) }
@@ -10,18 +11,51 @@ end
 
 post '/:mode/import/transmissions' do
   content_type "text/plain"
-  
-  file_path = params['datafile'][:tempfile].path
-  csv_parser = params[:mode] == 'tv' ? CsvParserTv.new : CsvParserRadio.new
-  csv_parser.parse_file(file_path)
-  
-  File.exists?(csv_parser.log_file_path) ? File.read(csv_parser.log_file_path) : "Log not found"
+  parse_csv(params['datafile'][:tempfile].path, params[:mode] == 'tv' ? CsvParserTv.new : CsvParserRadio.new)
 end
 
 post '/:mode/import/transmissions/clear' do
   content_type "text/plain"
-  
   Transmission.delete_all(type: params[:mode])
-  
   "Cleared all #{params[:mode]} transmission entries"
+end
+
+
+get '/:mode/import/stations' do
+  instructions = ''
+  erb :"import/import_csv", :locals => { :mode => params[:mode], :upload_type => 'Stations', :instructions => instructions, :breadcrumbs => get_breadcrumbs_by_mode(params[:mode], [{ :label => "Import", :link => "/#{params[:mode]}/import"}, { :label => "Stations", :link => "/#{params[:mode]}/import/stations"}]) }
+end
+
+post '/:mode/import/stations' do
+  content_type "text/plain"
+  parse_csv(params['datafile'][:tempfile].path, CsvParserStation.new)
+end
+
+post '/:mode/import/stations/clear' do
+  content_type "text/plain"
+  Station.delete_all(type: params[:mode])
+  "Cleared all #{params[:mode]} station entries"
+end
+
+
+get '/:mode/import/fillers' do
+  instructions = ''
+  erb :"import/import_csv", :locals => { :mode => params[:mode], :upload_type => 'Fillers', :instructions => instructions, :breadcrumbs => get_breadcrumbs_by_mode(params[:mode], [{ :label => "Import", :link => "/#{params[:mode]}/import"}, { :label => "Fillers", :link => "/#{params[:mode]}/import/fillers"}]) }
+end
+
+post '/:mode/import/fillers' do
+  content_type "text/plain"
+  parse_csv(params['datafile'][:tempfile].path, CsvParserFiller.new)
+end
+
+post '/:mode/import/fillers/clear' do
+  content_type "text/plain"
+  Filler.delete_all(type: params[:mode])
+  "Cleared all #{params[:mode]} filler entries"
+end
+
+
+def parse_csv(file_path, csv_parser)
+  csv_parser.parse_file(file_path)
+  File.exists?(csv_parser.log_file_path) ? File.read(csv_parser.log_file_path) : "Log not found"
 end
--- tests/routes/import_tests.rb
+++ tests/routes/import_tests.rb
@@ -1,13 +1,18 @@
 require_relative '../test_helper.rb'
 require_relative '../models/transmission_tests.rb'
+require_relative '../models/station_tests.rb'
+require_relative '../models/filler_tests.rb'
 require_relative '../helpers/csv_parser_radio_test.rb'
 require_relative '../helpers/csv_parser_tv_test.rb'
+require_relative '../helpers/csv_parser_station_test.rb'
+require_relative '../helpers/csv_parser_filler_test.rb'
 
 class ImportTest < Test::Unit::TestCase
   
   def teardown
     Transmission.delete_all
   end
+  
   
   def test_import_transmissions_it_contains_upload_control
     it_contains_upload_control("tv", "transmissions")
@@ -36,6 +41,63 @@ class ImportTest < Test::Unit::TestCase
     assert_equal(0, Transmission.where(type: 'tv').count)
   end
   
+  
+  def test_import_stations_it_contains_upload_control
+    it_contains_upload_control("tv", "stations")
+    it_contains_upload_control("radio", "stations")
+  end
+  
+  def test_import_stations_post
+    Station.delete_all(type: 'tv')
+    
+    post "/tv/import/stations", "datafile" => Rack::Test::UploadedFile.new(CsvParserStationTest::SAMPLE_FILE_FULL_PATH, "text/csv")
+    assert Station.where(type: 'tv').count == 2
+    
+    Station.delete_all(type: 'radio')
+    
+    post "/radio/import/stations", "datafile" => Rack::Test::UploadedFile.new(CsvParserStationTest::SAMPLE_FILE_FULL_PATH, "text/csv")
+    assert Station.where(type: 'radio').count == 2
+  end
+  
+  def test_import_stations_clear
+    StationTest.add_samples
+    
+    post "/radio/import/stations/clear"
+    assert_equal(0, Station.where(type: 'radio').count)
+    
+    post "/tv/import/stations/clear"
+    assert_equal(0, Station.where(type: 'tv').count)
+  end
+
+
+  def test_import_fillers_it_contains_upload_control
+    it_contains_upload_control("tv", "fillers")
+    it_contains_upload_control("radio", "fillers")
+  end
+  
+  def test_import_fillers_post
+    Filler.delete_all(type: 'tv')
+    
+    post "/tv/import/fillers", "datafile" => Rack::Test::UploadedFile.new(CsvParserFillerTest::SAMPLE_FILE_FULL_PATH, "text/csv")
+    assert Filler.where(type: 'tv').count == 1
+    
+    Filler.delete_all(type: 'radio')
+    
+    post "/radio/import/fillers", "datafile" => Rack::Test::UploadedFile.new(CsvParserFillerTest::SAMPLE_FILE_FULL_PATH, "text/csv")
+    assert Filler.where(type: 'radio').count == 1
+  end
+  
+  def test_import_fillers_clear
+    FillerTest.add_samples
+    
+    post "/radio/import/fillers/clear"
+    assert_equal(0, Filler.where(type: 'radio').count)
+    
+    post "/tv/import/fillers/clear"
+    assert_equal(0, Filler.where(type: 'tv').count)
+  end
+
+
   def it_contains_upload_control(mode, upload_type)
     get "/#{mode}/import/#{upload_type}"
     assert last_response.ok?

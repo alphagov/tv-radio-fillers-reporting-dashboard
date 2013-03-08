7a8665d35b2d288f42927ce43495ae614bb514b9
Steven Alexander <steven.william.alexander@googlemail.com>
2013-03-08 09:40:38 +0000

    rearranged import tests

--- tests/routes/import_tests.rb
+++ tests/routes/import_tests.rb
@@ -9,44 +9,37 @@ class ImportTest < Test::Unit::TestCase
     Transmission.delete_all
   end
   
-  def test_tv_transmissions_it_contains_upload_control
+  def test_import_transmissions_it_contains_upload_control
     it_contains_upload_control("tv", "transmissions")
-  end
-  def test_radio_transmissions_it_contains_upload_control
     it_contains_upload_control("radio", "transmissions")
   end
-  def it_contains_upload_control(mode, upload_type)
-    get "/#{mode}/import/#{upload_type}"
-    assert last_response.ok?
-    assert last_response.body.include?('<input type="file"')
-  end
   
-  def test_tv_transmissions_import_post
+  def test_import_transmissions_post
     Transmission.delete_all(type: 'tv')
     
     post "/tv/import/transmissions", "datafile" => Rack::Test::UploadedFile.new(CsvParserTvTest::SAMPLE_FILE_FULL_PATH, "text/csv")
     assert Transmission.where(type: 'tv').count == 10
-  end
-  
-  def test_radio_transmissions_import_post
+    
     Transmission.delete_all(type: 'radio')
     
     post "/radio/import/transmissions", "datafile" => Rack::Test::UploadedFile.new(CsvParserRadioTest::SAMPLE_FILE_FULL_PATH, "text/csv")
     assert Transmission.where(type: 'radio').count == 12
   end
   
-  def test_tv_transmissions_import_clear
+  def test_import_transmissions_clear
     TransmissionTest.add_samples
+    
+    post "/radio/import/transmissions/clear"
+    assert_equal(0, Transmission.where(type: 'radio').count)
     
     post "/tv/import/transmissions/clear"
     assert_equal(0, Transmission.where(type: 'tv').count)
   end
   
-  def test_radio_transmissions_import_clear
-    TransmissionTest.add_samples
-    
-    post "/radio/import/transmissions/clear"
-    assert_equal(0, Transmission.where(type: 'radio').count)
+  def it_contains_upload_control(mode, upload_type)
+    get "/#{mode}/import/#{upload_type}"
+    assert last_response.ok?
+    assert last_response.body.include?('<input type="file"')
   end
   
 end

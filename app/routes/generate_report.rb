59b1dc46afa57e80448dcd0d3a508aec1363bb20
Steven Alexander <steven.william.alexander@googlemail.com>
2013-04-09 13:52:02 +0100

    removed transmission query from generate reports

--- app/routes/generate_report.rb
+++ app/routes/generate_report.rb
@@ -1,8 +1,8 @@
 
 get '/tv/generate-report' do
-  erb :generate_report, :locals => { :mode => "tv", :breadcrumbs => get_breadcrumbs_tv([{ :label => "Reports", :link => "/tv/reports"}]), :data => query_for_params_with_paging('tv', params) }
+  erb :generate_report, :locals => { :mode => "tv", :breadcrumbs => get_breadcrumbs_tv([{ :label => "Reports", :link => "/tv/reports"}]), :data => nil }
 end
 
 get '/radio/generate-report' do
-  erb :generate_report, :locals => { :mode => "radio", :breadcrumbs => get_breadcrumbs_radio([{ :label => "Reports", :link => "/radio/reports"}]), :data => query_for_params_with_paging('radio', params) }
+  erb :generate_report, :locals => { :mode => "radio", :breadcrumbs => get_breadcrumbs_radio([{ :label => "Reports", :link => "/radio/reports"}]), :data => nil }
 end
